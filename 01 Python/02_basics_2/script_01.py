5c983069e24465144c064fc67145d02a69fb878e
Michal Radomski <michalra@op.pl>
2025-11-11 22:42:58 +0100

    Python course - commit 23

--- 01 Python/02_basics_2/script_01.py
+++ 01 Python/02_basics_2/script_01.py
@@ -295,3 +295,42 @@ while i < len(my_list):
     pass  # Does nothing!
 
 print("No error")  # No error
+
+# * Exercise!
+# * Display the image below to the right hand side where the 0 is going to be ' ', and the 1 is going to be '*'. This will reveal an image!
+picture = [
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 1, 1, 1, 0, 0],
+    [0, 1, 1, 1, 1, 1, 0],
+    [1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0],
+]
+
+# Answer:
+for row in picture:
+    for pixel in row:
+        if pixel:
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print("")
+#    *
+#   ***
+#  *****
+# *******
+#    *
+#    *
+#    *
+
+# * Exercise Find Duplicates
+some_list = ["a", "b", "c", "b", "d", "m", "n", "n"]
+duplicates = []
+
+for value in some_list:
+    if some_list.count(value) > 1:
+        if value not in duplicates:
+            duplicates.append(value)
+
+print(duplicates)  # ['b', 'n']
