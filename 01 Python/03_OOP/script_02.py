88dd010f44d50f8adecc23dc10edf47693959daa
Michal Radomski <michalra@op.pl>
2025-11-15 19:26:16 +0100

    Python course - commit 34

--- 01 Python/03_OOP/script_02.py
+++ 01 Python/03_OOP/script_02.py
@@ -19,7 +19,7 @@ player1.speak()  # My name is Tom and I am 20 years old.
 
 # * Inheritance
 # Parent Class
-class User:
+class User:  # User accepts object as parent class class User(object)
     def sign_in(self):
         print("logged in")
 
@@ -48,6 +48,175 @@ archer1 = Archer("Robbin", 100)
 wizard1.attack()  # Attacking with power of 50
 archer1.attack()  # Attacking with arrows: Arrows left- 100
 
+#  Isinstance(subclass, parent class)
 print(isinstance(wizard1, Wizard))  # True
 print(isinstance(wizard1, User))  # True
 print(isinstance(wizard1, object))  # True
+print(isinstance(wizard1, Archer))  # False
+
+"""
+By default every class is a subclass of 'object' class, hence when we type 'instance.' (object_name dot), we get all those
+default methods suggestions from the IDE.
+"""
+
+
+# * Polymorphism
+class User:
+    def signed_in(self):
+        print("User is logged in.")
+
+    def attack(self):
+        print("Do nothing.")
+
+
+class Wizard(User):
+    def __init__(self, name, power):
+        self.name = name
+        self.power = power
+
+    def attack(self):
+        User.attack(self)  # Just a way to use parent class method.
+        print(f"{self.name} is attacking with {self.power} power.")
+
+
+class Archer(User):
+    def __init__(self, name, arrow):
+        self.name = name
+        self.arrow = arrow
+
+    def attack(self):
+        print(f"{self.name} is attacking with {self.arrow} arrows.")
+
+
+wizard1 = Wizard("John", 50)
+archer1 = Archer("Mohan", 85)
+
+
+def player_attack(char_):
+    char_.attack()
+
+
+player_attack(wizard1)
+# Do nothing.
+# John is attacking with 50 power.
+player_attack(archer1)  # Mohan is attacking with 85 arrows.
+# notice that these two instances are executing the child class method and not the parent class method.
+
+for char in [wizard1, archer1]:
+    char.attack()
+# Do nothing.
+# John is attacking with 50 power.
+# Mohan is attacking with 85 arrows.
+
+"""
+This is polymorphism.
+Even though we are using the same function, it is giving different output based on the object.
+"""
+
+
+# * Exercise
+class Pets:
+    animals = []
+
+    def __init__(self, animals):
+        self.animals = animals
+
+    def walk(self):
+        for animal in self.animals:
+            print(animal.walk())
+
+
+class Cat:
+    is_lazy = True
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def walk(self):
+        return f"{self.name} is just walking around"
+
+
+class Simon(Cat):
+    def sing(self, sounds):
+        return f"{sounds}"
+
+
+class Sally(Cat):
+    def sing(self, sounds):
+        return f"{sounds}"
+
+
+# 1 Add another Cat
+class Chilli(Cat):
+    def sing(self, sounds):
+        return f"{sounds}"
+
+
+# 2 Create a list of all the pets (create 3 cat instances from the above).
+my_cats = [Simon("Simon", 5), Sally("Sally", 7), Chilli("Chilli", 3)]
+
+# 3 Instantiate the Pet class with all your cats use variable my_pets.
+my_pets = Pets(my_cats)
+print(my_pets)
+
+# 4 Output all the cats walking using the my_pets instance.
+my_pets.walk()
+# "Simon" is just walking around
+# "Sally" is just walking around
+# "Chilli" is just walking around
+
+
+class Pets:
+    animals = []
+
+    def __init__(self, animals):
+        self.animals = animals
+
+    def walk(self):
+        for animal in self.animals:
+            print(animal.walk())
+
+
+class Cat:
+    is_lazy = True
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def walk(self):
+        return f"{self.name} is just walking around"
+
+
+class Simon(Cat):
+    def sing(self, sounds):
+        return f"{sounds}"
+
+
+class Sally(Cat):
+    def sing(self, sounds):
+        return f"{sounds}"
+
+
+# 1 Add another Cat
+class Chilli(Cat):
+    def sing(self, sounds):
+        return f"{sounds}"
+
+
+# 2 Create a list of all the pets (create 3 cat instances from the above).
+simon = Simon("Simon", 3)
+sally = Sally("Sally", 2)
+chilli = Chilli("Chilli", 4)
+
+
+# 3 Instantiate the Pet class with all your cats use variable my_pets.
+all_pets = [simon, sally, chilli]
+print(my_pets)  # <__main__.Pets object at 0x75e08ea4b470>y
+
+# 4 Output all the cats walking using the my_pets instance.
+my_pets.walk()
+# "Simon" is just walking around
+# "Sally" is just walking around
+# "Chilli" is just walking around
