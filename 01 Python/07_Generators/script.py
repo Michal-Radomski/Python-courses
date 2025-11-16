11801937ac0ae0180562bd735f8b56ea950a1687
Michal Radomski <michalra@op.pl>
2025-11-16 22:10:52 +0100

    Python course - commit 51

--- 01 Python/07_Generators/script.py
+++ 01 Python/07_Generators/script.py
@@ -135,3 +135,34 @@ for i in gen:
 """
 loops by default deal with StopIteration error. they have build in functionality to handle them.
 """
+
+
+# * Exercise
+def fib(number):
+    n1 = 0
+    n2 = 1
+    for i in range(number):
+        yield n1
+        temp = n1
+        n1 = n2
+        n2 = temp + n2
+
+
+for x in fib(10000):
+    print(x)
+
+
+def fib2(num):
+    a = 0
+    b = 1
+    li = []
+    for i in range(num):
+        li.append(a)
+        temp = a
+        a = b
+        b = temp + b
+    print(li)
+
+
+num = int(input("Enter a number: "))
+fib2(num)
