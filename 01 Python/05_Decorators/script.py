9ec98edcc60db70ad7138190da0131788e3ffd36
Michal Radomski <michalra@op.pl>
2025-11-16 20:10:29 +0100

    Python course - commit 47

--- 01 Python/05_Decorators/script.py
+++ 01 Python/05_Decorators/script.py
@@ -108,3 +108,29 @@ def long_fn():
 
 
 long_fn()  # It took 0.3145303726196289 sec
+
+# * Exercise2
+# Create an @authenticated decorator that only allows the function to run is user1 has 'valid' set to True:
+user1 = {
+    "name": "Sorna",
+    "valid": True,  # changing this will either run or not run the message_friends function.
+}
+
+
+def authenticated(fn):
+    # code here
+    def wrapper(*args, **kwargs):
+        if args[0]["valid"]:
+            return fn(*args, **kwargs)
+        else:
+            return print("invalid user")
+
+    return wrapper
+
+
+@authenticated
+def message_friends(user):
+    print(f"message has been sent to: {user["name"]}")
+
+
+message_friends(user1)  # message has been sent to: Sorna
