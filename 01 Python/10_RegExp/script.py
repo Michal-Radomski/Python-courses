cb296ecffa91d7600bf83160e5c316c9e70d1f57
Michal Radomski <michalra@op.pl>
2025-11-18 21:01:40 +0100

    Python course - commit 63

---
+++ 01 Python/10_RegExp/script.py
@@ -0,0 +1,38 @@
+import re
+
+string = "this is a really cool string really!"
+
+a = re.search("really", string)
+print(a)  # <re.Match object; span=(10, 16), match='really'>
+
+# the below 4 commands will give error if the searching string does not exist.
+print(a.span())  # (10, 16)
+print(a.start())  # 10
+print(a.end())  # 16
+print(a.group())  # really
+
+pattern = re.compile("really")
+
+b = pattern.search(string)
+c = pattern.findall(string)
+
+pattern = re.compile("this is a really cool string really!")
+d = pattern.fullmatch("this is a really cool string really!")
+e = pattern.fullmatch(
+    "hello this is a really cool string really!"
+)  # this should be exact match, otherwise returns none
+
+pattern = re.compile("really")
+f = pattern.match(
+    "really cool feature"
+)  # it starts matching from the first character otherwise returns none
+g = pattern.match("yo really")
+
+print(f"b: {b}")  # b: <re.Match object; span=(10, 16), match='really'>
+print(f"c: {c}")  # c: ['really', 'really']
+print(
+    f"d: {d}"
+)  # d: <re.Match object; span=(0, 36), match='this is a really cool string really!'>
+print(f"e: {e}")  # e: None
+print(f"f: {f}")  # f: <re.Match object; span=(0, 6), match='really'>
+print(f"g: {g}")  # g: None
