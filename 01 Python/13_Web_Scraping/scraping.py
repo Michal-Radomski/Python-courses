7332b55238d22f642b83e611e0a27e327abad6b7
Michal Radomski <michalra@op.pl>
2025-11-21 20:36:35 +0100

    Python course - commit 7

---
+++ 01 Python/13_Web_Scraping/scraping.py
@@ -0,0 +1,36 @@
+import requests
+from bs4 import BeautifulSoup
+import pprint  # pretty print - used to print with appropriate spacing, for readability
+
+url = "https://news.ycombinator.com/"
+res = requests.get(url)
+
+# print(res)  # Response 200 means everything is good
+# print(res.text)
+# this will have the entire html data in it. Exactly the same thing which we get when we click 'View Page Source'
+
+soup = BeautifulSoup(res.text, "html.parser")
+
+# print(soup)
+# this will also have the exact same thing as res.text, but it will keep in html format, and not in string format,
+# and it will be easier to manipulate it
+
+# print(soup.body)
+# print(
+#     soup.body.contents
+# )  # result comes in a 'list' in this case. But not with the previous cases.
+
+# print(soup.find_all("div"))
+# print(soup.find_all("a"))  # find all the 'a' tags - all the links
+# print(soup.title)
+# print(soup.a)  # it finds the first a tag
+# print(soup.find("a"))  # it finds the first a tag
+
+# print(soup.find(id="score_24273602"))
+# print(soup.select("#score_24273602"))  # outputs in a list
+# select grabs the data using a CSS selector, where '.' means 'class', '#' means 'id', etc.
+
+# print(soup.select("a")[0])
+# grabs all the 'a' tags, and print only the first one, as this is a list, and we want the 0th item
+
+print(soup.select(".score")[0])  # grabs all the class="score" tags
