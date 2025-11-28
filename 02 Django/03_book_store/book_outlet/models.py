df9a792bd062265e5ffea51bbbe5c119c9c34007
Michal Radomski <michalra@op.pl>
2025-11-28 22:35:46 +0100

    Python Django course - commit 14

--- 02 Django/03_book_store/book_outlet/models.py
+++ 02 Django/03_book_store/book_outlet/models.py
@@ -1,4 +1,4 @@
-from django.core import validators
+# from django.core import validators
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.urls import reverse
