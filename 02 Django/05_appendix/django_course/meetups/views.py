81d5a746cfb01f8eaaf84950a507c7e4cf926796
Michal Radomski <michalra@op.pl>
2025-12-04 21:13:29 +0100

    Python Django course - commit 31

--- 02 Django/05_appendix/django_course/meetups/views.py
+++ 02 Django/05_appendix/django_course/meetups/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+# from django.http import HttpResponse
 
 # Create your views here.
 
 
 def index(request):
-    print(request)
-    return HttpResponse("Hello World!")
+    # print(request) #* <WSGIRequest: GET '/meetups/'>
+    # return HttpResponse("Hello World!")
+    return render(request, "meetups/index.html")
