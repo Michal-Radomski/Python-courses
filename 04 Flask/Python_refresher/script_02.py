046ca41b7876390ed1aa5d12194ba15668207898
Michal Radomski <michalra@op.pl>
2025-12-08 22:26:42 +0100

    Flask course - commit 09

--- 04 Flask/Python_refresher/script_02.py
+++ 04 Flask/Python_refresher/script_02.py
@@ -99,6 +99,8 @@ print(
     friends
 )  # [{'name': 'Rolf Smith', 'age': 24}, {'name': 'Adam Wool', 'age': 30}, {'name': 'Anne Pun', 'age': 27}]
 
+print(friends[1]["name"])  # Adam Wool
+
 # -- Iteration --
 student_attendance = {"Rolf": 96, "Bob": 80, "Anne": 100}
 for student in student_attendance:
@@ -123,3 +125,50 @@ else:
 # -- Calculate an average with `.values()` --
 attendance_values = student_attendance.values()
 print(sum(attendance_values) / len(attendance_values))  # 92.0
+
+# * Destructuring variables
+x, y = 5, 11
+# x, y = (5, 11)
+print("x,y:", x, y)  # x,y: 5 11
+
+# -- Destructuring in for loops --
+student_attendance = {"Rolf": 96, "Bob": 80, "Anne": 100}
+for student, attendance in student_attendance.items():
+    print(f"{student}: {attendance}")
+# Rolf: 96
+# Bob: 80
+# Anne: 100
+
+# -- Another example --
+people = [("Bob", 42, "Mechanic"), ("James", 24, "Artist"), ("Harry", 32, "Lecturer")]
+for name, age, profession in people:
+    print(f"Name: {name}, Age: {age}, Profession: {profession}")
+# Name: Bob, Age: 42, Profession: Mechanic
+# Name: James, Age: 24, Profession: Artist
+# Name: Harry, Age: 32, Profession: Lecturer
+
+# -- Much better than this! --
+for person in people:
+    print(f"Name: {person[0]}, Age: {person[1]}, Profession: {person[2]}")
+# Name: Bob, Age: 42, Profession: Mechanic
+# Name: James, Age: 24, Profession: Artist
+# Name: Harry, Age: 32, Profession: Lecturer
+
+# -- Ignoring values with underscore --
+person = ("Bob", 42, "Mechanic")
+name, _, profession = person
+print(name, profession)  # Bob Mechanic
+
+
+# -- Collecting values --
+head, *tail = [1, 2, 3, 4, 5]
+
+print(head)  # 1
+print(tail)  # [2, 3, 4, 5]
+
+*head, tail = [1, 2, 3, 4, 5]
+print(head)  # [1, 2, 3, 4]
+print(tail)  # 5
+
+# -- Packing and unpacking --
+# This isn't actually the end of the story, as there are also ways to pack and unpack collections using * and **, but that's a bit more advanced and you'll learn about it later on!
