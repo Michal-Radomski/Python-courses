7100825674777124ecd2e35bba24e5ee25fa05b2
Michal Radomski <michalra@op.pl>
2025-12-08 20:37:21 +0100

    Flask course - commit 06

--- 04 Flask/Python_refresher/script_01.py
+++ 04 Flask/Python_refresher/script_01.py
@@ -173,3 +173,24 @@ else:
 friends = ["Rolf", "Bob", "Jen"]
 print("Jen" in friends)  # True
 # * The `in` keyword works in most sequences like lists, tuples, and sets.
+
+movies_watched = {"The Matrix", "Green Book", "Her"}
+user_movie = input("Enter something you've watched recently: ")
+
+if user_movie in movies_watched:
+    print(f"I've watched {user_movie} too!")
+else:
+    print("I haven't watched that yet.")
+
+# --
+number = 7
+user_input = input("Enter 'y' if you would like to play: ")
+
+if user_input in ("y", "Y"):
+    user_number = int(input("Guess our number: "))
+    if user_number == number:
+        print("You guessed correctly!")
+    elif number - user_number in (1, -1):
+        print("You were off by 1.")
+    else:
+        print("Sorry, it's wrong!")
