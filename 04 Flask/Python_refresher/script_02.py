de6f84ad71b0b47394ab174fef551c3522279b57
Michal Radomski <michalra@op.pl>
2025-12-08 22:13:54 +0100

    Flask course - commit 08

--- 04 Flask/Python_refresher/script_02.py
+++ 04 Flask/Python_refresher/script_02.py
@@ -82,3 +82,44 @@ print(friends == starts_s)  # True
 print(
     "friends: ", id(friends), " starts_s: ", id(starts_s)
 )  # friends:  129593678312640  starts_s:  129593677757952
+
+# * Dictionaries
+friend_ages = {"Rolf": 24, "Adam": 30, "Anne": 27}
+friend_ages["Bob"] = 20
+print(friend_ages)  # {'Rolf': 24, 'Adam': 30, 'Anne': 27, 'Bob': 20}
+print(friend_ages["Bob"])  # 20
+
+# -- List of dictionaries --
+friends = [
+    {"name": "Rolf Smith", "age": 24},
+    {"name": "Adam Wool", "age": 30},
+    {"name": "Anne Pun", "age": 27},
+]
+print(
+    friends
+)  # [{'name': 'Rolf Smith', 'age': 24}, {'name': 'Adam Wool', 'age': 30}, {'name': 'Anne Pun', 'age': 27}]
+
+# -- Iteration --
+student_attendance = {"Rolf": 96, "Bob": 80, "Anne": 100}
+for student in student_attendance:
+    print(f"{student}: {student_attendance[student]}")
+# Rolf: 96
+# Bob: 80
+# Anne: 100
+
+# Better
+for student, attendance in student_attendance.items():
+    print(f"{student}: {attendance}")
+# Rolf: 96
+# Bob: 80
+# Anne: 100
+
+# -- Using the `in` keyword --
+if "Bob" in student_attendance:
+    print(f"Bob: {student_attendance[student]}")  # Bob: 100
+else:
+    print("Bob isn't a student in this class!")
+
+# -- Calculate an average with `.values()` --
+attendance_values = student_attendance.values()
+print(sum(attendance_values) / len(attendance_values))  # 92.0
