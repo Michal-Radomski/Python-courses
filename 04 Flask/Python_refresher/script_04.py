1530af56788c7e8383c1fb05b204e2122f3f434d
Michal Radomski <michalra@op.pl>
2025-12-10 23:01:59 +0100

    Flask course - commit 18

--- 04 Flask/Python_refresher/script_04.py
+++ 04 Flask/Python_refresher/script_04.py
@@ -124,3 +124,54 @@ class Store:
 
 
 # * @classmethod and @staticmethod
+class ClassTest:
+    def instance_method(self):
+        print(f"Called instance_method of {self}")
+
+    @classmethod
+    def class_method(cls):
+        print(f"Called class_method of {cls}")
+
+    @staticmethod
+    def static_method():
+        print(f"Called static_method. We don't get any object or class info here.")
+
+
+instance = ClassTest()
+instance.instance_method()  # Called instance_method of <__main__.ClassTest object at 0x77e0f89ee210>
+
+ClassTest.class_method()  # Called class_method of <class '__main__.ClassTest'>
+ClassTest.static_method()  # Called static_method. We don't get any object or class info here.
+
+
+# -- What are they used for? --
+# Instance methods are used for most things. When you want to produce an action that uses the data stored in an object.
+# Static methods are used to just place a method inside a class because you feel it belongs there (i.e. for code organization, mostly!)
+# Class methods are often used as factories.
+
+
+class Book:
+    TYPES = ("hardcover", "paperback")
+
+    def __init__(self, name, book_type, weight):
+        self.name = name
+        self.book_type = book_type
+        self.weight = weight
+
+    def __repr__(self):
+        return f"<Book {self.name}, {self.book_type}, weighing {self.weight}g>"
+
+    @classmethod
+    def hardcover(cls, name, page_weight):
+        return cls(name, cls.TYPES[0], page_weight + 100)
+
+    @classmethod
+    def paperback(cls, name, page_weight):
+        return cls(name, cls.TYPES[1], page_weight)
+
+
+heavy = Book.hardcover("Harry Potter", 1500)
+light = Book.paperback("Python 101", 600)
+
+print(heavy)  # <Book Harry Potter, hardcover, weighing 1600g>
+print(light)  # <Book Python 101, paperback, weighing 600g>
