11dfe1ae87713e68484e4aefb27654fd735f31c9
Michal Radomski <michalra@op.pl>
2025-12-10 21:55:04 +0100

    Flask course - commit 15

--- 04 Flask/Python_refresher/script_03.py
+++ 04 Flask/Python_refresher/script_03.py
@@ -319,3 +319,43 @@ def apply(*args, operator):
 
 print(apply(1, 3, 6, 7, operator="+"))  # 17
 # print(apply(1, 3, 5, "+"))  # Error
+
+
+# * Unpacking keyword arguments
+# -- Unpacking kwargs --
+def named(**kwargs):
+    print(kwargs)
+
+
+named(name="Bob", age=25)  # {'name': 'Bob', 'age': 25}
+# named({"name": "Bob", "age": 25})  # Error, the dictionary is actually a positional argument.
+
+# Unpack dict into arguments. This is OK, but slightly more confusing. Good when working with variables though.
+named(**{"name": "Bob", "age": 25})
+
+
+# -- Unpacking and repacking --
+def named(**kwargs):
+    print(kwargs)
+
+
+def print_nicely(**kwargs):
+    named(**kwargs)  # Unpack the dictionary into keyword arguments.
+    for arg, value in kwargs.items():
+        print(f"{arg}: {value}")
+
+
+print_nicely(name="Bob", age=25)
+
+
+# -- Both args and kwargs --
+def both(*args, **kwargs):
+    print(args)
+    print(kwargs)
+
+
+both(1, 3, 5, name="Bob", age=25)
+# (1, 3, 5)
+# {'name': 'Bob', 'age': 25}
+# This is normally used to accept an unlimited number of arguments and keyword arguments, such that some of them can be passed onto other functions.
+# You'll frequently see things like these in Python code:
