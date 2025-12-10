f45ab37e4faaf5aebaa96687273d2e573dfb6e61
Michal Radomski <michalra@op.pl>
2025-12-10 22:38:38 +0100

    Flask course - commit 17

--- 04 Flask/Python_refresher/script_04.py
+++ 04 Flask/Python_refresher/script_04.py
@@ -98,3 +98,29 @@ bob = Person("Bob", 35)
 print(
     bob
 )  # Not as nice, but we could re-create "Bob" very easily. -> <Person('Bob', 35)>
+
+
+class Store:
+    def __init__(
+        self,
+        name,
+    ):
+        # You'll need 'name' as an argument to this method.
+        # Then, initialise 'self.name' to be the argument, and 'self.items' to be an empty list.
+        self.name = name
+        self.items = []
+
+    def add_item(self, name, price):
+        # Create a dictionary with keys name and price, and append that to self.items.
+        item = {"name": name, "price": price}
+        self.items.append(item)
+
+    def stock_price(self):
+        # Add together all item prices in self.items and return the total.
+        total = 0
+        for item in self.items:
+            total += item["price"]
+        return total
+
+
+# * @classmethod and @staticmethod
