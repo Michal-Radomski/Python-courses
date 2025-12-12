f0ad9b2f19ad49780a67e12d715fa05a55ff2f95
Michal Radomski <michalra@op.pl>
2025-12-12 20:52:14 +0100

    Flask course - commit 25

--- 04 Flask/Python_refresher/script_06.py
+++ 04 Flask/Python_refresher/script_06.py
@@ -216,3 +216,116 @@ def make_secure(func):
 @make_secure
 def get_admin_password():
     return "1234"
+
+
+# * Mutability in Python
+a = []
+b = a
+# Remember a and b are _names_ for the list. They both have the _same_ value.
+
+a.append(35)  # Modify the value.
+
+print(a)  # [35]
+print(b)  # [35]
+
+# We mutated (changed) the value, its names still point to the _same thing_, so it doesn't matter which name you use.
+
+a = []
+b = []
+
+a.append(35)
+
+print(a)  # [35]
+print(b)  # []
+
+# Here they are different lists, because [] creates a new list every time. You can check whether two things are the _same_ one by usingt the `id()` function:
+
+print(id(a))
+print(id(b))  # Different from id(a)
+
+# -- immutable --
+# Some values can't be changed because they don't have methods that modify the value itself.
+# In case of the list, `.append()` mutates the list.
+# For example integers don't have any such methods, so they are called _immutable_.
+
+a = 8597
+b = 8597
+
+print(id(a))
+print(id(b))  # Same one
+
+a = 8598
+
+print(id(a))
+print(
+    id(b)
+)  # Different, because we didn't change 8597. We just used the name 'a' for a different value. 'b' still is a name for 8597.
+
+# Most things are mutable in Python. If you want to keep one of your classes immutable, don't add any methods that change the objects' properties.
+
+# Tuples and strings are the only fundamental collection in Python which is immutable.
+# Lists, sets, dictionaries are all mutable.
+# Integers, floats, and booleans are all immutable.
+
+# -- += and similar --
+# A lot of beginners think this:
+a = "hello"
+b = a
+
+print(id(a))
+print(id(b))
+
+a += "world"
+
+# Would cause 'b' to change
+# But it doesn't, because strings are immutable. When you do str + str, a _new_ string is created.
+# This means that a becomes a new string containing "helloworld", but b still is a name for "hello".
+
+print(id(a))
+print(id(b))
+
+
+# * Mutable default parameters (and why they're a bad idea)
+# from typing import List
+
+
+# class Student:
+#     def __init__(self, name: str, grades: List[int] = []):  #* This is bad!
+#         self.name = name
+#         self.grades = grades
+
+#     def take_exam(self, result):
+#         self.grades.append(result)
+
+
+# bob = Student("Bob")
+# rolf = Student("Rolf")
+# bob.take_exam(90)
+# print(bob.grades)
+# print(rolf.grades)  # Whaaaaaat
+
+# The function parameters evaluate when the function is defined, not when it runs.
+# That means that self.grades is a name for the list that was evaluated when the function was defined.
+# We're then modifying it in take_exam
+# But all calls to the __init__ method have the same list (because parameters are only evaluated once!)
+# So all students have the same list
+
+# Avoid it by not having mutable parameters. Instead, do what we did in prior lectures:
+
+from typing import List
+
+
+class Student:
+    def __init__(self, name: str, grades: List[int] = None):
+        self.name = name
+        self.grades = grades or []  # * New list created if one isn't passed
+
+    def take_exam(self, result):
+        self.grades.append(result)
+
+
+bob = Student("Bob")
+rolf = Student("Rolf")
+bob.take_exam(90)
+print(bob.grades)
+print(rolf.grades)  # Now it's empty.
