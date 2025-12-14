d5b9ece6d585d83d6ec0307e6a9bdcb4e0da17a1
Michal Radomski <michalra@op.pl>
2025-12-14 22:11:27 +0100

    Flask course - commit 31

--- 04 Flask/03/app.py
+++ 04 Flask/03/app.py
@@ -2,11 +2,9 @@ from flask import Flask  # type: ignore[import-not-found]
 from flask_smorest import Api  # type: ignore[import-not-found]
 
 from db import db
-
-import models
-
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
+from resources.tag import blp as TagBlueprint
 
 
 def create_app(db_url=None):
@@ -30,5 +28,6 @@ def create_app(db_url=None):
 
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(StoreBlueprint)
+    api.register_blueprint(TagBlueprint)
 
     return app
