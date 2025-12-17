235d8b7255ccdefb8c82937f72cf566015066be1
Michal Radomski <michalra@op.pl>
2025-12-17 23:05:15 +0100

    FastApi course - commit 05

--- 05 FastApi/01/main.py
+++ 05 FastApi/01/main.py
@@ -1,8 +1,13 @@
-from fastapi import FastAPI  # type: ignore[import-not-found]
+# from fastapi import FastAPI  # type: ignore[import-not-found]
+from fastapi import FastAPI
+from routers.posts import router as posts_router
+
+# app = FastAPI()
+# @app.get("/")
+# async def root():
+#     return {"message": "Hello World"}
+
 
 app = FastAPI()
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+# print("app:", app) # app: <fastapi.applications.FastAPI object at 0x757e51b34e60>
+app.include_router(posts_router)
