b0a3fac54d22c33fb94e6dd1e90a2eb061a1acd1
Michal Radomski <michalra@op.pl>
2025-12-20 23:53:54 +0100

    FastApi course - commit 14

---
+++ 05 FastApi/02_proj_(not_working)/main.py
@@ -0,0 +1,54 @@
+# from fastapi import FastAPI  # type: ignore[import-not-found]
+import logging
+from contextlib import asynccontextmanager
+
+from database import database
+from fastapi import FastAPI, HTTPException
+from fastapi.exception_handlers import http_exception_handler
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+from logging_conf import configure_logging
+from routers.posts import router as posts_router
+from routers.user import router as user_router
+
+# app = FastAPI()
+# @app.get("/")
+# async def root():
+#     return {"message": "Hello World"}
+
+# * V1
+# app = FastAPI()
+# print("app:", app) # app: <fastapi.applications.FastAPI object at 0x757e51b34e60>
+
+logger = logging.getLogger(__name__)
+
+
+# + Test not working, code not working!
+# * V2
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    configure_logging()
+    await database.connect()
+    yield
+    await database.disconnect()
+
+
+app = FastAPI(lifespan=lifespan)
+
+app.include_router(posts_router)
+app.include_router(user_router)
+
+
+@app.exception_handler(HTTPException)
+async def http_exception_handle_logging(request, exc):
+    logger.error(f"HTTPException: {exc.status_code} {exc.detail}")
+    return await http_exception_handler(request, exc)
+
+
+# * Favicon
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+@app.get("/favicon.ico", include_in_schema=False)
+async def favicon():
+    return FileResponse("static/favicon.svg")
---
+++ 05 FastApi/02_proj_(not_working)/tests/conftest.py
@@ -0,0 +1,56 @@
+import os
+from typing import AsyncGenerator, Generator
+
+import pytest  # type: ignore[import-not-found]
+from fastapi.testclient import TestClient  # type: ignore[import-not-found]
+from httpx import ASGITransport, AsyncClient  # type: ignore[import-not-found]
+
+os.environ["ENV_STATE"] = "test"
+
+
+# from routers.posts import comments_table, post_table
+from database import database, user_table
+from main import app
+
+
+@pytest.fixture(scope="session")
+def anyio_backend():
+    return "asyncio"
+
+
+@pytest.fixture()
+def client() -> Generator:
+    yield TestClient(app)
+
+
+# @pytest.fixture(autouse=True)
+@pytest.fixture(autouse=True)
+async def db() -> AsyncGenerator:
+    await database.connect()
+    yield
+    await database.disconnect()
+
+
+@pytest.fixture()
+async def async_client(client) -> AsyncGenerator:
+    async with AsyncClient(
+        transport=ASGITransport(app),
+        base_url=client.base_url,
+    ) as ac:
+        yield ac
+
+
+@pytest.fixture()
+async def registered_user(async_client: AsyncClient) -> dict:
+    user_details = {"email": "test@example.net", "password": "1234"}
+    await async_client.post("/register", json=user_details)
+    query = user_table.select().where(user_table.c.email == user_details["email"])
+    user = await database.fetch_one(query)
+    user_details["id"] = user.id
+    return user_details
+
+
+@pytest.fixture()
+async def logged_in_token(async_client: AsyncClient, registered_user: dict) -> str:
+    response = await async_client.post("/token", json=registered_user)
+    return response.json()["access_token"]
