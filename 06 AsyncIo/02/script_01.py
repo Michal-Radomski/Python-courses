f62e1c26945383efb9dfc88b5a0197a4800026d2
Michal Radomski <michalra@op.pl>
2025-12-30 20:58:56 +0100

    Asyncio course - commit 05

---
+++ 06 AsyncIo/02/script_01.py
@@ -0,0 +1,19 @@
+import asyncio
+
+
+async def greet(name, delay):
+    await asyncio.sleep(delay)
+    print(f"hello {name}, after {delay} seconds!")
+
+
+async def main():
+    task1 = asyncio.create_task(greet("Alice", 1))
+    task2 = asyncio.create_task(greet("Bob", 2))
+
+    await task1
+    await task2
+
+
+asyncio.run(main())
+# hello Alice, after 1 seconds!
+# hello Bob, after 2 seconds!
