9253936759b04b850090c9de9a15b1853a2bdeea
Michal Radomski <michalra@op.pl>
2026-03-24 22:51:36 +0100

    Raspberry Pi course - commit 06

--- 09 RaspberryPi/02/01.py
+++ 09 RaspberryPi/02/01.py
@@ -1,4 +1,5 @@
 # Blinking LED Python Code
+
 import time
 
 import RPi.GPIO as GPIO  # type: ignore[import-not-found]
@@ -6,8 +7,13 @@ import RPi.GPIO as GPIO  # type: ignore[import-not-found]
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(21, GPIO.OUT)
 
-while True:
-    GPIO.output(21, GPIO.HIGH)
-    time.sleep(1)
-    GPIO.output(21, GPIO.LOW)
-    time.sleep(1)
+try:
+    while True:
+        GPIO.output(21, GPIO.HIGH)
+        time.sleep(1)
+        GPIO.output(21, GPIO.LOW)
+        time.sleep(1)
+except KeyboardInterrupt:  # Handles Ctrl+C gracefully
+    print("Stopped by user")
+finally:
+    GPIO.cleanup()  # Resets pin 21 to safe input state
---
+++ 09 RaspberryPi/02/02.py
@@ -0,0 +1,24 @@
+# Traffic Lights Control System Code
+# Todo: add GPIO.cleanup()
+
+import time
+
+import RPi.GPIO as GPIO  # type: ignore[import-not-found]
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(36, GPIO.OUT)
+GPIO.setup(38, GPIO.OUT)
+GPIO.setup(40, GPIO.OUT)
+while True:
+    GPIO.output(36, GPIO.HIGH)
+    GPIO.output(38, GPIO.LOW)
+    GPIO.output(40, GPIO.LOW)
+    time.sleep(1)
+    GPIO.output(36, GPIO.LOW)
+    GPIO.output(38, GPIO.HIGH)
+    GPIO.output(40, GPIO.LOW)
+    time.sleep(1)
+    GPIO.output(36, GPIO.LOW)
+    GPIO.output(38, GPIO.LOW)
+    GPIO.output(40, GPIO.HIGH)
+    time.sleep(1)
