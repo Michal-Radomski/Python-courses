645ac730f2777c962f6c3e2d9f8c77251e45cbc0
Michal Radomski <michalra@op.pl>
2026-03-24 21:13:43 +0100

    Raspberry Pi course - commit 05

---
+++ 09 RaspberryPi/02/01.py
@@ -0,0 +1,13 @@
+# Blinking LED Python Code
+import time
+
+import RPi.GPIO as GPIO  # type: ignore[import-not-found]
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(21, GPIO.OUT)
+
+while True:
+    GPIO.output(21, GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(21, GPIO.LOW)
+    time.sleep(1)
