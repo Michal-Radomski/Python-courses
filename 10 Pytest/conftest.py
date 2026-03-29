33f7d1c8b08a90489480d7985005b1115b7fc95e
Michal Radomski <michalra@op.pl>
2026-03-29 22:35:50 +0200

    Testing With Pytest course - commit 14

---
+++ 10 Pytest/conftest.py
@@ -0,0 +1,21 @@
+import os
+
+import pytest  # type: ignore[import-not-found]
+
+QA_config = "qa.prop"
+prod_config = "prod.prop"
+
+
+def pytest_addoption(parser):
+    parser.addoption("--cmdopt", default="QA")
+
+
+@pytest.fixture()
+def CmdOpt(pytestconfig):
+    print("\n In CmdOpt fixture function")
+    opt = pytestconfig.getoption("cmdopt")
+    if opt == "Prod":
+        f = open(os.path.join(os.path.dirname(__file__), prod_config), "r")
+    else:
+        f = open(os.path.join(os.path.dirname(__file__), QA_config), "r")
+    yield f
